eb29559cb81399fe39a8ca8eaad2a69882a85281
jiawei666 <957089263@qq.com>
2021-03-15 14:08:56 +0800

    新增composer 新增14

--- 13. PHP socket初探 --- 硬着头皮继续libevent（二）.php
+++ 13. PHP socket初探 --- 硬着头皮继续libevent（二）.php
@@ -3,20 +3,20 @@
 /**
  * event用法初体验
  */
-//// 初始化一个EventConfig（舰岛），虽然是个仅用于演示的空配置
-//$eventConfig = new EventConfig();
-//// 根据EventConfig初始化一个EventBase（辽宁舰，根据舰岛配置下辽宁舰）
-//$eventBase = new EventBase( $eventConfig );
-//// 初始化一个定时器event（歼15，然后放到辽宁舰机库中）
-//$timer = new Event( $eventBase, -1, Event::TIMEOUT, function(){
-//    echo microtime( true )." : 歼15，滑跃，起飞！".PHP_EOL;
-//} );
-//// tick间隔为0.05秒钟，我们还可以改成0.5秒钟甚至0.001秒，也就是毫秒级定时器
-//$tick = 0.01;
-//// 将定时器event添加（将歼15拖到甲板加上弹射器）
-//$timer->add( $tick );
-//// eventBase进入loop状态（辽宁舰！走你！）
-//$eventBase->loop();
+// 初始化一个EventConfig（舰岛），虽然是个仅用于演示的空配置
+$eventConfig = new EventConfig();
+// 根据EventConfig初始化一个EventBase（辽宁舰，根据舰岛配置下辽宁舰）
+$eventBase = new EventBase( $eventConfig );
+// 初始化一个定时器event（歼15，然后放到辽宁舰机库中）
+$timer = new Event( $eventBase, -1, Event::TIMEOUT |  Event::PERSIST, function(){
+    echo microtime( true )." : 歼15，滑跃，起飞！".PHP_EOL;
+} );
+// tick间隔为0.05秒钟，我们还可以改成0.5秒钟甚至0.001秒，也就是毫秒级定时器
+$tick = 0.01;
+// 将定时器event添加（将歼15拖到甲板加上弹射器）
+$timer->add( $tick );
+// eventBase进入loop状态（辽宁舰！走你！）
+$eventBase->loop();
 
 /**
  * event进阶（接收处理信号）
@@ -57,16 +57,16 @@
 /**
  * event进阶（config配置）
  */
-$base = new EventBase();
-echo "特性：".PHP_EOL;
-$features = $base->getFeatures();
-// 看不到这个判断条件的，请反思自己“位运算”相关欠缺
-if( $features & EventConfig::FEATURE_ET ){
-    echo "边缘触发".PHP_EOL;
-}
-if( $features & EventConfig::FEATURE_O1 ){
-    echo "O1添加删除事件".PHP_EOL;
-}
-if( $features & EventConfig::FEATURE_FDS ){
-    echo "任意文件描述符，不光socket".PHP_EOL;
-}
+//$base = new EventBase();
+//echo "特性：".PHP_EOL;
+//$features = $base->getFeatures();
+//// 看不到这个判断条件的，请反思自己“位运算”相关欠缺
+//if( $features & EventConfig::FEATURE_ET ){
+//    echo "边缘触发".PHP_EOL;
+//}
+//if( $features & EventConfig::FEATURE_O1 ){
+//    echo "O1添加删除事件".PHP_EOL;
+//}
+//if( $features & EventConfig::FEATURE_FDS ){
+//    echo "任意文件描述符，不光socket".PHP_EOL;
+//}
